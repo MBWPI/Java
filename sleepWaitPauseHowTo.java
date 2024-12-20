f946a60de93b4e8b0d61a8000a3117067c8fe07e
MBWPI <124680046+MBWPI@users.noreply.github.com>
2024-12-20 01:31:37 -0500

    Create sleepWaitPauseHowTo.java

---
+++ sleepWaitPauseHowTo.java
@@ -0,0 +1,11 @@
+class Main {
+  public static void main(String[] args) throws InterruptedException {
+    // Add "throws InterruptedException" to work
+
+    // Sleeps/Waits/Pauses for 5 seconds
+    System.out.println("\nSleeping for 5 seconds...");
+    Thread.sleep(5000);
+    System.out.println("\nSlept for 5 seconds.");
+    
+  }
+}
