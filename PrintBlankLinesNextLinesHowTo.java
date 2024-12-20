35a47e8ffc31a9ed8bda87be9a45f83608346449
MBWPI <124680046+MBWPI@users.noreply.github.com>
2024-12-20 01:14:28 -0500

    Create PrintBlankLinesNextLinesHowTo.java

---
+++ PrintBlankLinesNextLinesHowTo.java
@@ -0,0 +1,16 @@
+class Main {
+  public static void main(String[] args) {
+    
+    // Printing, blank lines, and next lines
+    System.out.print("\nSystem.out.print prints text");
+
+    // \n in text to skip lines
+    System.out.print("\n\nAdd backslash and letter n within the quotation marks to go to the next line\n");
+
+    // Blank line after a print
+    System.out.println();
+    System.out.println("System.out.println prints a blank line after the print\n");
+    // System.out.print() does not work/error occurs
+    
+  }
+}
