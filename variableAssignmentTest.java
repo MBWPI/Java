581405a0197b3f8b6792e63b1421d34cf8cceb23
MBWPI <124680046+MBWPI@users.noreply.github.com>
2024-12-20 01:37:50 -0500

    Create variableAssignmentTest.java

---
+++ variableAssignmentTest.java
@@ -0,0 +1,19 @@
+class Main {
+  public static void main(String[] args) {
+    
+    int x = 3;
+    int y = 4;
+
+    System.out.println("x is " + x);
+    System.out.println("y is " + y);
+
+    y = x;
+
+    System.out.println("y is " + y);
+
+    x = 7;
+
+    System.out.println("y is " + y);
+
+  }
+}
